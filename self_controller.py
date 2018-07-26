a8739d52b9374d84f0eeeb0e21a1146c21ae4d1e
Asher Merrill <asher.s.merrill@gmail.com>
2018-07-26 00:43:08 -0400

    self_controller now fully working
    Also added some example programs that I don't want to use too late into the evening.

--- self_controller.py
+++ self_controller.py
@@ -5,8 +5,14 @@ A simple script to limit the user's abilities past a certain time of day.
 Reads programs from a file.  Program names in this file can be used to:
     Halt shutdown: Computer will not shutdown or interfere with user if
         program is on this list.
-    Kill program: User will be notified that the program on kill list
-        will be terminated in some ammount of time; program is then killed.
+    Shutdown machine: When a processes on no_prompt BUT NOT on halt_shutdown is
+        found, a shutdown in 5 minutes is started.
+
+'no_prompt' processes are processes that the user has already been asked about,
+thus does not need to be prompted about again.
+
+'halt_shutdown' processes are processes the user has designated to halt
+shutdown.
 
 Future functionality: When a window goes fullscreen, user is prompted if
 that window should be banned or not once past time.
@@ -16,8 +22,12 @@ that window should be banned or not once past time.
 import ctypes
 import datetime
 import json
-import psutil
+import os
+import subprocess
+import sys
 import time
+
+import psutil
 import win32gui
 import win32process
 
@@ -38,12 +48,33 @@ ID_ABORT = 3
 ID_YES = 6
 ID_NO = 7
 
-EARLIEST = datetime.time(7)
-LATEST = datetime.time(15)
+########################
+# Times:
+EARLIEST = datetime.time(7) # Hours
+LATEST = datetime.time(23)
 
-F = 'prx.json' # Note that no_prompt is a superset of halt_shutdown
-# In other words, all items on halt_shutdown should be on no_prompt
+########################
+# Process list file:
+F = 'prx_list.json'
 
+Fdir = os.path.dirname(sys.argv[0])
+F = os.path.join(Fdir, F)
+
+# Make sure F exists:
+if not os.path.exists(F):
+    with open(F, 'w') as f:
+        json.dump({'halt_shutdown':[], 'no_prompt':[]}, f)
+
+# Open the process list, then initialize the globals, then close it:
+with open(F, 'r') as f:
+    raw_prx_list = json.load(f)
+    halt_shutdown = raw_prx_list['halt_shutdown']
+    no_prompt = raw_prx_list['no_prompt']
+
+response = ctypes.windll.user32.MessageBoxW(None,
+                                                "You're being watched.",
+                                                "Self Controller",
+                                                MB_OK | ICON_INFO)
 
 
 def legal_time(now):
@@ -61,18 +92,13 @@ def legal_time(now):
         raise TypeError('{} is not a datetime.datetime or'
                         ' datetime.time!'.format(now))
 
-
 def legal_prx(foreground):
     """ Determines if ``foreground`` is a permitted process by reading
     permissible list from prx.json file.
 
     :param foreground: process to judge
     """
-    with open(F, 'r') as f:
-        halt_shutdown, _ = json.load(f) # First obj is halt_shutdown, second
-        # obj is no_prompt
     return foreground in halt_shutdown
-
 
 def prompt(foreground):
     """ Asks user if ``foreground`` is a legal process.
@@ -84,58 +110,85 @@ def prompt(foreground):
                                                 format(foreground),
                                                 "Self Controller",
                                                 MB_YESNO | ICON_INFO)
-    # Read file:
-    with open(F, 'r') as f:
-        halt_shutdown, no_prompt = json.load(f)
-    if response == ID_YES: # If good, append it to the good list:
+    # Get these from global space:
+    global halt_shutdown, no_prompt
+    if response == ID_YES: # Yes, append it to the good list:
         halt_shutdown.append(foreground)
     no_prompt.append(foreground)
-    # Write file:
-    with open(F, 'w') as f:
-        json.dump((halt_shutdown, no_prompt), f, indent=4, sort_keys=True)
-
 
 def prompted(foreground):
-    # Read file:
-    with open(F, 'r') as f:    
-        _, no_prompt = json.load(f)
     if foreground not in no_prompt:
         prompt(foreground)
     return True
 
-
 def shutdown():
     """ Tells the system to shutdown. """
-    print('SHUTTING DOWN')
-
+    return subprocess.run(['shutdown', '-s', '-t', '120'])
 
 def shutdown_query():
-    """ Determines if the conditions to shutdown have been met. """
+    """ Determines if the conditions to shutdown have been met.
+
+    These are order specific, as:
+        Have we asked about this process before?
+        Is this process alowed?
+        Are we inside legal time?
+
+    Program will wait 1 minute and see if the same application is still open.
+    If it is, the machine will be told to shutdown.
+    """
     foreground = active_window_process_name()
     now = datetime.datetime.now().time()
     # If all the conditions are met, shutdown:  (Note this is order-specific)
     if prompted(foreground) and not legal_prx(foreground)\
     and not legal_time(now):
-        shutdown()
-
+        stop_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
+        iter_time = datetime.datetime.now()
+        # While still timing...
+        while iter_time < stop_time:
+            time.sleep(0.05)
+            # If the user leaves the bad active window...
+            if foreground != active_window_process_name():
+                # Reset (return None...)
+                return None
+            # Update time for the loop...
+            iter_time = datetime.datetime.now()
+        # Otherwise...
+        return shutdown()
+    # We didn't satisfy the conditions to begin with, so leave...
+    return None
 
 def active_window_process_name():
     """ Function to return the process name of the current executable.
     Amazingly taken from: https://stackoverflow.com/questions/14394513/
         win32gui-get-the-current-active-application-name
     """
-    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
-    return psutil.Process(pid[-1]).name()
+    while True:
+        try:
+            pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
+            pname = psutil.Process(pid[-1]).name()
+        except:
+            pass
+        else:
+            return pname
 
 
 def main():
-    """ OOGGITY BOOGITY """
+    """ Main execution loop. """
     while True:
-        time.sleep(3)
-        shutdown_query()
+        time.sleep(0.1)
+        response = shutdown_query()
+        if response and response.returncode != 0:
+            raise ValueError('Shutdown returned non-zero exit code: '
+                               '{}'.format(response.returncode))
+        elif response:
+            break
 
 
-main()
-
-
-F.close()
+try:
+    main()
+except Exception as e:
+    print('WARNING: Exception caught:\n{}'.format(e))
+finally:
+    with open(F, 'w') as f:
+        json.dump({'halt_shutdown':halt_shutdown, 'no_prompt':no_prompt},
+                  f, indent=4, sort_keys=True)
