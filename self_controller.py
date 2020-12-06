289b6394c1046bbe91b23426bfd4084f59274c1e
Asher Merrill <asher.s.merrill@gmail.com>
2020-12-06 00:39:48 -0400

    self controller: avoid shutdown when halt condition true

--- self_controller.py
+++ self_controller.py
@@ -7,8 +7,11 @@ Created on Sat Dec  5 14:18:25 2020
 
 import ctypes
 import datetime as dt
+import json
 import subprocess
 import time
+
+import win32com.client
 
 # ctypes promtps:
 MB_OK = 0x0
@@ -43,8 +46,27 @@ badtimes = (  # badtimes[0] <= bad <= badtimes[-1]
 shutdown = 'shutdown -s -t 60'
 shutdown_abort = 'shutdown -a'
 
+processfile = 'processes.json'
+
+
+# Open halt process file:
+with open(processfile, 'r') as fp:
+    halt_processes = json.load(fp)
+
+
+def running_processes():
+    """Get running processes."""
+    wmi = win32com.client.GetObject('winmgmts:')
+    return {p.Name for p in wmi.InstancesOf('win32_process')}
+
+
+def halt():
+    """Check if any halt processes are active."""
+    return any([(i in halt_processes) for i in running_processes()])
+
 
 def main():
+    """Wait for shutdown time, check if okay then shutdown."""
     # Wait until it's a bad time:
     now = dt.datetime.now()
     if not badtimes[0] <= now.time() <= badtimes[-1]:
@@ -65,6 +87,11 @@ def main():
         time.sleep(sleeptime)
 
     while True:
+        # Check if it's time to shutdown:
+        while halt() is True:
+            print('Shutdown-halting process open.  Waiting...')
+            time.sleep(1.)
+
         # Then start shutdown clock:
         print('Trying shutdown...')
         subprocess.run(shutdown.split())
@@ -72,13 +99,15 @@ def main():
         # Ask if the user wants a delay:
         response = ctypes.windll.user32.MessageBoxW(
             None,
-            "Shutdown scheduled...\nSnooze for 10 minutes?",
+            "Shutdown scheduled in 60 seconds.\t\t\t\t\n\nSnooze for 10 minutes?",
             "Self Controller Improved",
             MB_YESNO | ICON_INFO | DEF_BUT2
         )
         if response == ID_YES:
             subprocess.run(shutdown_abort.split())
-            print(f'Sleeping for another 600 seconds for snooze...')
+            print(
+                'User requested snooze.  Sleeping for another 600 seconds for snooze...'
+            )
             time.sleep(600)
         else:
             print('Shutdown not aborted, closing...')
