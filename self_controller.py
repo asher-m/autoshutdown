dd2aec23bdbd7551f5000e107c8960b3ea9fda14
Asher Merrill <asher.s.merrill@gmail.com>
2020-12-21 23:20:56 -0500

    self controller: change how times are ingested
    * makes script more reliable by using full datetimes
    * no longer gives dialog to delay shutdown (for even less self control)

--- self_controller.py
+++ self_controller.py
@@ -5,114 +5,99 @@ Created on Sat Dec  5 14:18:25 2020
 @author: asher
 """
 
-import ctypes
-import datetime as dt
+import datetime
 import json
 import os
+import re
 import subprocess
 import time
 
 import psutil
 
-# ctypes promtps:
-MB_OK = 0x0
-MB_OKCXL = 0x01
-MB_YESNOCXL = 0x03
-MB_YESNO = 0x04
-MB_HELP = 0x4000
 
-# ctypes icons:
-ICON_EXLAIM = 0x30
-ICON_INFO = 0x40
-ICON_STOP = 0x10
-
-# ctypes default buttons:
-DEF_BUT1 = 0x000  # default to button 1
-DEF_BUT2 = 0x100  # default to button 2, etc...
-DEF_BUT3 = 0x200
-DEF_BUT4 = 0x300
-
-# ctypes responses:
-ID_OK = 0
-ID_CANCEL = 2
-ID_ABORT = 3
-ID_YES = 6
-ID_NO = 7
-
-badtimes = (  # badtimes[0] <= bad <= badtimes[-1]
-    dt.time(0, 30),
-    dt.time(4, 30)
-)
-
-shutdown = 'shutdown -s -t 60'
-shutdown_abort = 'shutdown -a'
-
-processfile = os.path.join(os.path.dirname(__file__), 'processes.json')
-
-
-# Open halt process file:
-with open(processfile, 'r') as fp:
-    halt_processes = json.load(fp)
-
-
-def running_processes():
-    """Get running processes."""
-    return {proc.name() for proc in psutil.process_iter()}
-
-
-def halt():
-    """Check if any halt processes are active."""
-    return any([(i in halt_processes) for i in running_processes()])
+shutdown = r'shutdown -s -t {}'
 
 
 def main():
     """Wait for shutdown time, check if okay then shutdown."""
     # Wait until it's a bad time:
-    now = dt.datetime.now()
-    if not badtimes[0] <= now.time() <= badtimes[-1]:
-        if now.time() < badtimes[0]:
-            # Sleep until later today (case after midnight before bedtime):
-            sleeptime = (dt.datetime.combine(dt.date.today(), badtimes[0]) -
-                         dt.datetime.combine(dt.date.today(), now.time())).total_seconds()
-        else:  # now.time() > badtimes[-1]:
-            badtime_tomorrow = dt.datetime.combine(
-                now + dt.timedelta(days=1),
-                badtimes[0]
-            )
-            # Sleep until tomorrow's bedtime (ie., after midnight):
-            sleeptime = (badtime_tomorrow - now).total_seconds()
+    now = datetime.datetime.now()
 
-        # Sleep for that time...
-        print(f'Sleeping for {sleeptime} seconds...')
-        time.sleep(sleeptime)
+    if bedtime <= now <= wuptime:
+        sleeptime = 0
+    else:
+        sleeptime = (bedtime - now).total_seconds()
 
-    while True:
-        # Check if it's time to shutdown:
-        while halt() is True:
-            print('Shutdown-halting process open.  Waiting...')
-            time.sleep(5)
+    print(f'Sleeping for {int(sleeptime)} seconds...')
+    time.sleep(sleeptime)
 
-        # Then start shutdown clock:
-        print('Trying shutdown...')
-        subprocess.run(shutdown.split())
+    if delay() is True:
+        wait_for_shutdown = 30 * 60  # seconds
+        print(f'Got delay, setting shutdown in {int(wait_for_shutdown / 60):d}'
+              ' minutes...')
+    else:
+        wait_for_shutdown = 3 * 60  # seconds
+        print(f'Got no delay, setting shutdown in {int(wait_for_shutdown / 60):d}'
+              ' minutes...')
+    subprocess.run(shutdown.format(wait_for_shutdown).split())
 
-        # Ask if the user wants a delay:
-        response = ctypes.windll.user32.MessageBoxW(
-            None,
-            "Shutdown scheduled in 60 seconds.\t\t\t\t\n\nSnooze for 10 minutes?",
-            "Self Controller Improved",
-            MB_YESNO | ICON_INFO | DEF_BUT2
-        )
-        if response == ID_YES:
-            subprocess.run(shutdown_abort.split())
-            print(
-                'User requested snooze.  Sleeping for another 600 seconds for snooze...'
-            )
-            time.sleep(600)
-        else:
-            print('Shutdown not aborted, closing...')
-            break
+    input('Press ENTER to close...')
+
+
+def get_times(timelines):
+    """Decide bedtime and wuptime from timelines.
+
+    Works by:
+    1: Look if wuptime can mean today (ie., wuptime >= now) it is, else tomorrow.
+    2: If bedtime on the day of wuptime is <= wuptime, it is, else it is the day before."""
+    today = datetime.datetime.now().date()
+    tomorrow = today + datetime.timedelta(days=1)
+    now = datetime.datetime.now()
+    # decide day of wuptime
+    m_wuptime = re.match(r'wuptime:(\d{1,2}):(\d{1,2})', timelines[1])
+    t_wuptime = datetime.time(int(m_wuptime.group(1)), int(m_wuptime.group(2)))
+    if datetime.datetime.combine(today, t_wuptime) >= now:
+        wuptime = datetime.datetime.combine(today, t_wuptime)
+    else:
+        wuptime = datetime.datetime.combine(tomorrow, t_wuptime)
+
+    # decide day of bedtime
+    m_bedtime = re.match(r'bedtime:(\d{1,2}):(\d{1,2})', timelines[0])
+    t_bedtime = datetime.time(int(m_bedtime.group(1)), int(m_bedtime.group(2)))
+
+    if datetime.datetime.combine(wuptime.date(), t_bedtime) <= wuptime:
+        bedtime = datetime.datetime.combine(wuptime.date(), t_bedtime)
+    else:
+        bedtime = datetime.datetime.combine(
+            wuptime.date() - datetime.timedelta(days=1), t_bedtime)
+
+    return bedtime, wuptime
+
+
+def get_processes(processlines):
+    """Get processes in process file lines.
+
+    Make them lowercase to compare to running processes."""
+    return {p.lower() for p in processlines}
+
+
+def get_processes_running():
+    """Get running processes.
+
+    Make them lowercase to compare to processes in process file."""
+    return {proc.name().lower() for proc in psutil.process_iter()}
+
+
+def delay():
+    """Check if any halt processes are active."""
+    return any([(p in processes) for p in get_processes_running()])
 
 
 if __name__ == '__main__':
+    with open(os.path.join(os.path.dirname(__file__), 'times.txt'), 'r') as fp:
+        bedtime, wuptime = get_times(fp.readlines())
+
+    with open(os.path.join(os.path.dirname(__file__), 'processes.txt'), 'r') as fp:
+        processes = get_processes(fp.readlines())
+
     main()
